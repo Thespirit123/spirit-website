b6baa048cb0bf188fb8a1ec3faf81e04d97f00fb
TaladeogoA <ataladeogo@gmail.com>
2025-01-08 17:30:39 +0100

    Enhance HeroSection with responsive images for better mobile and desktop display

--- src/components/page-sections/home/hero/index.tsx
+++ src/components/page-sections/home/hero/index.tsx
@@ -1,20 +1,20 @@
 import HeroImg from "@/assets/images/home-hero-bg.png";
+import HeroScreensMobile from "@/assets/images/home-hero-screens-mobile.png";
 import HeroScreens from "@/assets/images/home-hero-screens.png";
 import Button from "@/components/custom-ui/button";
-import { Text } from "@/components/custom-ui/text";
 import Image from "next/image";
 
 export const HeroSection = () => (
-  <section className="min-h-[480px] h-[calc(100vh-120px)] flex">
+  <section className="min-h-[480px] h-[calc(100vh-120px)] flex overflow-hidden">
     <Image src={HeroImg} alt="Hero Image" layout="fill" objectFit="cover" />
     <div className="w-11/12 max-w-7xl mx-auto z-10 mt-20 relative">
-      <Text variant="h1" className="text-white text-center font-medium">
+      <h1 className="text-white text-center font-medium text-3xl sm:text-4xl md:text-4xl lg:text-5xl leading-tight">
         Innovative Digital Solutions <br /> at Your
         <span className="text-brand-primary"> Fingertips</span>
-      </Text>
-      <Text variant="h3" className="text-white text-center mt-2 font-normal">
+      </h1>
+      <h3 className="text-white text-center mt-2 sm:mt-4 font-normal text-lg sm:text-xl md:text-xl lg:text-2xl">
         Cutting-Edge Mobile Apps & Services
-      </Text>
+      </h3>
       <Button
         variant="primary"
         className="mt-6 mx-auto block"
@@ -23,15 +23,27 @@ export const HeroSection = () => (
         Explore Our Products
       </Button>
 
-      <div className="mx-auto mt-[7rem] absolute bottom-0 left-0 right-0">
-        <Image
-          src={HeroScreens}
-          alt="Hero Screens"
-          width={1200}
-          height={800}
-          className="w-3/5 h-auto mx-auto"
-          priority
-        />
+      <div className="w-full mt-[7rem] absolute bottom-0 left-0 right-0">
+        <div className="hidden lg:block">
+          <Image
+            src={HeroScreens}
+            alt="Hero Screens"
+            width={1200}
+            height={800}
+            className="w-[95%] sm:w-[85%] md:w-3/4 lg:w-3/5 h-auto mx-auto"
+            priority
+          />
+        </div>
+        <div className="block lg:hidden -mb-24 sm:-mb-56">
+          <Image
+            src={HeroScreensMobile}
+            alt="Hero Screens"
+            width={800}
+            height={600}
+            className="w-full h-auto mx-auto"
+            priority
+          />
+        </div>
       </div>
     </div>
   </section>
