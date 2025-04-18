230b263fc1548c85ea5a978fc2f6638f0e998fd8
Chukwubuike Kingsley <86000049+Buike369@users.noreply.github.com>
2025-04-18 06:14:41 +0100

    uploads

--- src/components/page-sections/home/FeaturedProducts/index.jsx
+++ src/components/page-sections/home/FeaturedProducts/index.jsx
@@ -159,8 +159,20 @@ const index = () => {
       />
             
             <div className='featuredProductCardBestSell'>{app.cartText}</div>
-            <div className='featuredProductCardAddToCart'>Add to Cart</div>
-            <div className='featuredProductCardLoveIcone'><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div className='featuredProductCardAddToCart'> <svg width="17" height="16" viewBox="0 0 17 16" fill="none" >
+<g clipPath="url(#clip0_709_899)">
+<path d="M5.57145 14.6663C5.93964 14.6663 6.23812 14.3679 6.23812 13.9997C6.23812 13.6315 5.93964 13.333 5.57145 13.333C5.20326 13.333 4.90479 13.6315 4.90479 13.9997C4.90479 14.3679 5.20326 14.6663 5.57145 14.6663Z" stroke="#F8FAFC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M12.9049 14.6663C13.2731 14.6663 13.5716 14.3679 13.5716 13.9997C13.5716 13.6315 13.2731 13.333 12.9049 13.333C12.5368 13.333 12.2383 13.6315 12.2383 13.9997C12.2383 14.3679 12.5368 14.6663 12.9049 14.6663Z" stroke="#F8FAFC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M1.60498 1.36621H2.93831L4.71165 9.64621C4.7767 9.94945 4.94543 10.2205 5.18879 10.4128C5.43215 10.605 5.73492 10.7064 6.04498 10.6995H12.565C12.8684 10.6991 13.1626 10.5951 13.399 10.4048C13.6354 10.2145 13.7997 9.94923 13.865 9.65288L14.965 4.69954H3.65165" stroke="#F8FAFC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_709_899">
+<rect width="16" height="16" fill="white" transform="translate(0.238281)"/>
+</clipPath>
+</defs>
+</svg>
+<span>Add to Cart</span></div>
+            <div className='featuredProductCardLoveIcone'><svg width="17" height="16" viewBox="0 0 17 16" fill="none">
 <g clipPath="url(#clip0_709_978)">
 <path d="M13.1668 9.33333C14.1602 8.36 15.1668 7.19333 15.1668 5.66667C15.1668 4.69421 14.7805 3.76158 14.0929 3.07394C13.4053 2.38631 12.4726 2 11.5002 2C10.3268 2 9.50016 2.33333 8.50016 3.33333C7.50016 2.33333 6.6735 2 5.50016 2C4.5277 2 3.59507 2.38631 2.90744 3.07394C2.2198 3.76158 1.8335 4.69421 1.8335 5.66667C1.8335 7.2 2.8335 8.36667 3.8335 9.33333L8.50016 14L13.1668 9.33333Z" stroke="#0F172A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 </g>
