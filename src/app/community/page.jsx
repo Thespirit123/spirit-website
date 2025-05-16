693ff2027b0e71f02c4067c8f8d91febe32b2e27
Chukwubuike Kingsley <86000049+Buike369@users.noreply.github.com>
2025-05-16 16:27:50 +0100

    uploads

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -5,7 +5,102 @@ import "./community.css"
 
 const page = () => {
   return (
+    <div style={{padding:"60px"}}>
+
+      <div>
+        <div>Nigeria's Premier Digital Community</div>
+        <div>The Spirit Community Rewards</div>
+        <div>Earn rewards, get exclusive discounts, and join Nigeria's fastest-growing digital community</div>
+        <div>
+          <div>Join Now</div>
+          <div>Learn More</div>
+        </div>
+        <div>
+           <div>
+            <div><span>5000+</span></div>
+            <div>Active Members</div>
+           </div>
+            <div>
+              <div><span>2000</span></div>
+               <div>Welcome Bonus</div>
+            </div>
+             <div>
+                <div><span>200+</span></div>
+                <div>Rewards Given</div>
+             </div>
+        </div>
+      </div>
+
+      <div>
+        <div>Earn & Redeem Rewards</div>
+        <div>Join our community rewards program and unlock exclusive benefits, discounts, and 
+special offers.</div>
+<div>
+  <div>
     <div>
+      <p>Your Rewards Journey</p>
+      <p>Collect points with every interaction and 
+unlock exclusive benefits.</p>
+    </div>
+    <div>
+      <div>
+        <div>350 points</div>
+        <div>500 points needed</div>
+      </div>
+      <div></div>
+      <div>
+        <div>Current Level</div>
+         <div>Next reward</div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div>
+      <div>
+        <div>icon</div>
+        <div>
+          <p>Ways To Earn</p>
+          <p>Multiple ways to earn points and 
+advance your membership level</p>
+        </div>
+      </div>
+      <div>Show More</div>
+    </div>
+    <div>
+       <div>
+        <div>icon</div>
+        <div>
+          <p>Ways To Redeem</p>
+          <p>Exchange your points for valuable 
+rewards and experiences</p>
+        </div>
+      </div>
+      <div>Show More</div>
+    </div>
+  </div>
+</div>
+      </div>
+
+    <div>
+      <div>
+      <div>Friend Referrals</div>
+      <div>Give your friends & family ₦2,000 off their first purchase. For each 
+successful referral, you'll receive ₦2,000 in rewards!</div>
+<div>
+  <div>icon</div>
+   <div>
+    <p></p>
+    <p></p>
+    
+   </div>
+</div>
+<div>
+  <div></div>
+   <div></div>
+</div>
+      </div>
+      <div></div>
+    </div>
 
     </div>
   )
