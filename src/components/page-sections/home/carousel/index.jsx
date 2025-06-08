a081e6219fa3519b12cac1c6b410ffadc28b8a89
Chukwubuike Kingsley <86000049+Buike369@users.noreply.github.com>
2025-06-08 19:37:30 +0100

    upolad

--- src/components/page-sections/home/carousel/index.jsx
+++ src/components/page-sections/home/carousel/index.jsx
@@ -14,12 +14,12 @@ import "react-multi-carousel/lib/styles.css";
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1.5,
+    items: 2,
     slidesToSlide: 1 // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 1.4,
+    items: 2,
     slidesToSlide: 1 // optional, default to 1.
   },
   mobile: {
